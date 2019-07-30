de91716811538cf39d6f93eb5903e7c612c8a4eb
alfianguide@outlook.com <alfianguide@outlook.com>
2019-07-30 18:37:08 +0700

    major update ( fish, mimeapp, nvim, omf )

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+export PATH="$HOME/go/bin:$PATH"
+export GOPATH="$HOME/go"
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+source $HOME/.cargo/env
+export PATH="$HOME/.cargo/bin:$PATH"
+alias vim="nvim"
+alias fm="sudo nnn"
+alias mdata="sudo mount /dev/sda5 /data"
+alias umdata="sudo umount /data"
+alias mand="fusermount -u cell/"
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/alfianguide/.local/share/omf/themes/spacefish/fish_prompt.fish
